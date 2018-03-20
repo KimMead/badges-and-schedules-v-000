52e68bc609962d28e574062921599277a651aff5
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-20 15:49:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creater(attendees)
+def batch_badge_creator(attendees)
   badge_messages_array = []
   attendees.each array << message
 end
