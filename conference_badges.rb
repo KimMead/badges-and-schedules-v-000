17fd483cc12e6fa0e3222f35a3c30695b1555acf
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-20 16:05:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,6 @@ attendees_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(array)
   array.each do |attendee|
-    "Hello, my name #{name}"
+    puts "Hello, my name #{name}"
   end
 end
