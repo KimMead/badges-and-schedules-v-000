a33e4e9b14fc8a2c1a4055ebf637ad252738ba56
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-20 16:57:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,7 @@ def batch_badge_creator(array)
   arr
 end
 
-room_array = [1,2,3,4,5,6,7]
-
 def assign_rooms(name, room)
-  arr = []
-  array.each_with_index
-
+  array.each_with_index(1) do |name, index|
+puts "Hello, #{name}!  You'll be assigned to room #{index}!"
 end
