bb3bfde5f549270ab0d4ed3f4b74d650c8c64600
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-20 17:11:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(array)
   arr2 = []
-  array.each_with_index { |name| arr2 << "Hello, #{name}! You'll be assigned to room #{index}!" }
+  array.each_with_index { |name| arr2 << "Hello, #{name}! You'll be assigned to room [0]!" }
   arr2
 end
